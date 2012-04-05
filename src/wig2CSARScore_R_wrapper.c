6101d889c0a1a4df9139d7e41a43d1aad2d1e970
Pedro Madrigal <pm@engineering.com>
2012-04-05 00:27:44 +0000

    wig2CSARScore update.Devel version is now 1.1.1
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/NarrowPeaks@64873 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/wig2CSARScore_R_wrapper.c
+++ src/wig2CSARScore_R_wrapper.c
@@ -5,9 +5,41 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 #include <R_ext/Print.h>
+#include <R_ext/Memory.h>
 
-int wig2CSARScore(char** file_Name,int* nbChr, int* chrL,char** filenames,char** chr);
+int wig2CSARScore(const char* file_Name,int* nbChr, int* chrL,char** filenames, char** chr);
 
-void wig2CSARScore_R_wrapper(char** file_Name,int* nbChr, int* chrL,char** filenames,char** chr, int *digits) {
-	*digits = wig2CSARScore(file_Name,nbChr,chrL,filenames,chr);
+SEXP wig2CSARScore_R_wrapper(SEXP file_Name, SEXP nbChr, SEXP chrL)
+{
+    int c_nbChr = INTEGER(nbChr)[0];
+    char /* R_alloc'd memory is automatically freed when we return to R */
+        **c_filenames = (char **) R_alloc(c_nbChr, sizeof(char *)),
+        **c_chr = (char **) R_alloc(c_nbChr, sizeof(char *));
+    SEXP result, r_filenames, r_chr, r_digits;
+
+    int c_digits = wig2CSARScore(CHAR(STRING_ELT(file_Name, 0)),
+                                 INTEGER(nbChr), INTEGER(chrL),
+                                 c_filenames, c_chr);
+
+    /* 'SEXP's need to be PROTECTed from garbage collection */
+    result = PROTECT(allocVector(VECSXP, 4)); /* explicit PROTECTion */
+    r_chr = allocVector(STRSXP, c_nbChr);
+    SET_VECTOR_ELT(result, 0, r_chr); /* 1st element -- chr; implicit PROTECTion */
+    SET_VECTOR_ELT(result, 1, chrL);  /* 2nd element -- chrL */
+    r_filenames = allocVector(STRSXP, c_nbChr),
+    SET_VECTOR_ELT(result, 2, r_filenames); /* 3rd element -- filenames */
+    r_digits = allocVector(REALSXP, 1);
+    SET_VECTOR_ELT(result, 3, r_digits); /* 4th element -- digits */
+
+    /* copy from C -> R */
+    for (int i = 0; i < c_nbChr; ++i) {
+        SET_STRING_ELT(r_chr, i, mkChar(c_chr[i]));
+        SET_STRING_ELT(r_filenames, i, mkChar(c_filenames[i]));
+    }
+    REAL(r_digits)[0] = c_digits;
+
+    /* clean up */
+    UNPROTECT(1);
+    return result;
 }
+
